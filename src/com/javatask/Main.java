5fb627335411349292114d92bda659ca123bf112
arunkumar-designer <prasannaarun98@gmail.com>
2022-06-26 23:08:17 +0530

    data type task

--- src/com/javatask/Main.java
+++ src/com/javatask/Main.java
@@ -5,5 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World, I'm Java");
+
+        //Task 1 (DataTypes)
+        task1 project1;
+        project1 =new task1();
+        System.out.println(project1.a);
+        System.out.println(project1.b);
+        System.out.println(project1.c);
+        System.out.println(project1.d);
+        System.out.println(project1.e);
+        System.out.println(project1.f);
+        System.out.println(project1.g);
+        System.out.println(project1.h);
+
+
     }
 }
