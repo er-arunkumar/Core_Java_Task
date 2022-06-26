627189cfe720e664cec3547d628611bce6fca835
arunkumar-designer <prasannaarun98@gmail.com>
2022-06-26 21:45:41 +0530

    Initial commit

---
+++ src/com/javatask/Main.java
@@ -0,0 +1,9 @@
+package com.javatask;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World, I'm Java");
+    }
+}
