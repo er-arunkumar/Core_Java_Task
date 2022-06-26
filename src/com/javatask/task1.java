e43faecbcaf32e99a7d80a3ebfc8d274c8d65999
arunkumar-designer <prasannaarun98@gmail.com>
2022-06-26 23:00:24 +0530

    data type task

---
+++ src/com/javatask/task1.java
@@ -0,0 +1,13 @@
+package com.javatask;
+
+public class task1 {
+    //Data Types
+    int a=25;
+    double b=1.23;
+    String c="A";
+    boolean d = true;
+    boolean e = false;
+    double f = 3.45f;
+    double g = 3.445e2;
+    String h ="arun";
+}
